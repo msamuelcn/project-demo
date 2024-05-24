81677048484827dc354f92302ff50ffc4532941f
Samuel Nicasio <47882653+msamuelcn@users.noreply.github.com>
2024-05-24 23:25:15 +0800

    Sample Home.py

--- Home.py
+++ Home.py
@@ -3,3 +3,13 @@ import streamlit as st
 st.title("Home")
 st.write("Provide a short description of the problem and your objective.")
 st.write("Provide a list of your names and github handles")
+
+# Add markdown
+st.title ("this is the app title")
+st.header("this is the markdown")
+st.markdown("this is the header")
+st.subheader("this is the subheader")
+st.caption("this is the caption")
+st.code("x=2021")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+
