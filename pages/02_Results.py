c86af7224ee4adee8d958ad0885a646e19a38aa1
Samuel Nicasio <47882653+msamuelcn@users.noreply.github.com>
2024-05-25 12:00:35 +0800

    Update 02_Results.py

--- pages/02_Results.py
+++ pages/02_Results.py
@@ -1,5 +1,23 @@
 import streamlit as st
 
-st.header("1. Top customer spending behavior by category")
-st.image("images/total_spending.png")
-st.text("Travel is the top spender's highest spending, followed by onsite grocery and onsite shopping.")
+st.subheader("Pre-profiling")
+st.image("images/8.jpg")
+
+st.subheader("Cluster #0: DIGITAL DYNAMOS")
+st.write("Top tier based on rfm, on average has the same spending for both digital and physical.")
+st.image("images/12.jpg")
+
+st.subheader("Cluster #3: CYBER SAVVY SHOPPERS")
+st.write("Next tier of overall spending has higher digital average than digital dynamos.")
+st.image("images/16.jpg")
+
+st.subheader("Cluster #5: FESTIVE SPENDERS")
+st.write("Occasional spenders who has the affinity to spend during holidays and summer.")
+st.image("images/20.jpg")
+
+st.subheader("Cluster #4: EPIC COMEBACK CONNOISSEURS")
+st.write("Infrequent spenders but when they come back, they are very high average spenders.")
+st.image("images/24.jpg")
+
+st.subheader("Post-profiling")
+st.image("images/25.jpg")
