b8b6fec889b81bc01ef5dc1bfc8184dc19aa34d7
Samuel Nicasio <47882653+msamuelcn@users.noreply.github.com>
2024-05-25 09:58:58 +0800

    Slides

--- Home.py
+++ Home.py
@@ -1,25 +1,35 @@
 import streamlit as st
 
-st.title("Home")
-st.write("Provide a short description of the problem and your objective.")
-st.write("Provide a list of your names and github handles")
-
-# Add markdown
-st.title ("this is the app title")
-st.header("this is the markdown")
-st.markdown("this is the header")
-st.subheader("this is the subheader")
-st.caption("this is the caption")
-st.code("x=2021")
-st.latex(r''' a+a r^1+a r^2+a r^3 ''')
-
 st.header("From Piggy Banks to Pin Codes")
 st.title("An Analysis on AAC’s Customers and their Spending Behaviors")
 st.caption('Presented By: Group 2 | Hungry Sleepers')
 
 st.header("Goals and Objectives")
-st.write('How do we better drive the business?')
-st.subheader('Customer demograhpics')
-st.markdown("- Customer demograhpics")
-st.markdown("- Spending behaviors")
-st.markdown("- What do they keep buying?")
+st.image("images/3.jpg")
+st.image("images/4.jpg")
+st.image("images/5.jpg")
+st.subheader("Methodology")
+st.image("images/6.jpg")
+st.subheader("Overview of customer demographics.")
+st.image("images/7.jpg")
+st.subheader("Pre-profiling")
+st.image("images/8.jpg")
+st.subheader("")
+st.image("images/12.jpg")
+
+st.image("images/16.jpg")
+
+st.image("images/20.jpg")
+
+st.image("images/24.jpg")
+st.subheader("Post-profiling")
+st.image("images/25.jpg")
+
+st.image("images/26.jpg")
+
+st.subheader("Summary")
+st.markdown("- For the entire dataset, Six (6) clusters were determined. Two (2) of which were considered disengaged while the remaining Four (4) were subjected to further profiling.")
+st.markdown(" - Digital Dynamo consisted of heavy frequent spenders with around same average spending for digital and physical")
+st.markdown(" - Festive Spenders were considered as ‘Festive Spenders’ since their average spendings peaked during the holidays")
+st.markdown(" - Epic Comeback Connoisseurs were made up infrequent, but heavy spenders")
+st.markdown("- Campaigns were designed for each customer cluster based on their spending behaviors to encourage increased digital transactions.")
