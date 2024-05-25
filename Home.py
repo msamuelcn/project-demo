0f0a314ab0c82b4f690ab7dab65d26cb807e85e9
Samuel Nicasio <47882653+msamuelcn@users.noreply.github.com>
2024-05-25 11:52:13 +0800

    Summary edit

--- Home.py
+++ Home.py
@@ -39,8 +39,17 @@ st.image("images/25.jpg")
 st.image("images/26.jpg")
 
 st.subheader("Summary")
-st.markdown("- For the entire dataset, Six (6) clusters were determined. Two (2) of which were considered disengaged while the remaining Four (4) were subjected to further profiling.")
-st.markdown(" - Digital Dynamo consisted of heavy frequent spenders with around same average spending for digital and physical")
-st.markdown(" - Festive Spenders were considered as ‘Festive Spenders’ since their average spendings peaked during the holidays")
-st.markdown(" - Epic Comeback Connoisseurs were made up infrequent, but heavy spenders")
-st.markdown("- Campaigns were designed for each customer cluster based on their spending behaviors to encourage increased digital transactions.")
+# st.markdown("- For the entire dataset, **Six (6) clusters were determined. Two (2) of which were considered disengaged** while the remaining **Four (4) were subjected to further profiling**.")
+# st.markdown(" - **Digital Dynamo** consisted of heavy frequent spenders with around same average spending for digital and physical")
+# st.markdown(" - **Festive Spenders** were considered as ‘Festive Spenders’ since their average spendings peaked during the holidays")
+# st.markdown(" - **Epic Comeback Connoisseurs** were made up infrequent, but heavy spenders")
+# st.markdown("- **Campaigns** were designed for each customer cluster based on their spending behaviors to encourage increased digital transactions.")
+
+st.markdown("""
+* For the entire dataset, **Six (6) clusters were determined. Two (2) of which were considered disengaged** while the remaining **Four (4) were subjected to further profiling**.
+  * **Digital Dynamo** consisted of heavy frequent spenders with around same average spending for digital and physical.
+  * **Festive Spenders** were considered as ‘Festive Spenders’ since their average spendings peaked during the holidays.
+  *  **Epic Comeback Connoisseurs** were made up infrequent, but heavy spenders.
+  *  **Campaigns** were designed for each customer cluster based on their spending behaviors to encourage increased digital transactions.
+* **Campaigns** were designed for each customer cluster based on their spending behaviors to encourage increased digital transactions.  
+""")
